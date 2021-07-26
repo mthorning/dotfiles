24b74b2e5ce6dad9073fee7498d2eccf0111d7b2
Matt Thorning <matt.thorning@headforwards.com>
2021-07-26 08:52:25 +0100

    added xclip to install

--- install_1.sh
+++ install_1.sh
@@ -9,7 +9,7 @@ sudo apt update && sudo apt upgrade -y
 # install apt packages
 sudo apt install -y neovim zsh tmux curl fzf libjpeg8-dev zlib1g-dev python-dev \
 python3-dev libxtst-dev python3-pip libxft-dev make xorg xcape mlocate apt-transport-https \
-pkg-config libharfbuzz-dev xorg openbox bluez bluez-tools pavucontrol htop \
+pkg-config libharfbuzz-dev xorg openbox bluez bluez-tools pavucontrol htop xclip \
 wget stow chromium-browser i3 fonts-symbola flameshot laptop-mode-tools
 
 # install Oh-my-zsh
