64b7b91df3099b217df73ab5690804bc4ce45fdb
Matthew Thorning <matthew@ometria.com>
2022-08-03 09:11:06 +0100

    init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,24 +17,21 @@ vim.opt.incsearch = true
 vim.opt.showmode = false
 vim.opt.mouse = 'a'
 vim.opt.iskeyword = vim.opt.iskeyword:append('-')
-vim.opt.undodir = '$HOME/.vim/undodir'
+vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
 vim.opt.undofile = true
 vim.opt.timeoutlen = 0
 vim.opt.signcolumn = 'yes'
 vim.opt.syntax = 'on'
 
-vim.cmd([[
-  let g:tokyonight_italic_functions=1
-  let g:tokyonight_italic_keywords=0
-  colorscheme tokyonight
+vim.g.tokyonight_italic_functions = 1
+vim.g.tokyonight_italic_keywords = 0
+vim.cmd("colorscheme tokyonight")
 
-  let lazygit_floating_window_use_plenary=1
-]])
+vim.g.lazygit_floating_window_use_plenary = 1
 
 vim.cmd("autocmd BufEnter *.txt if &buftype == 'help' | wincmd L | endif")
 
 vim.cmd("autocmd FileType markdown setlocal spell spelllang=en_gb wrap")
-vim.cmd("autocmd FileType go setlocal sw=8")
 
 vim.cmd("command! LspInstallAll :call LspInstallAll()")
 
