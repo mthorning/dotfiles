c5cef2093fdb0d736e46fae500c20da7c292b755
Matthew Thorning <matthew@ometria.com>
2023-02-18 15:36:54 +0000

    tmux sessionizer from telescope ftw

--- nvim/lua/custom/tmux_server_telescope.lua
+++ nvim/lua/custom/tmux_server_telescope.lua
@@ -1,33 +1,31 @@
-local pickers = require "telescope.pickers"
-local finders = require "telescope.finders"
-local conf = require("telescope.config").values
-local actions = require "telescope.actions"
-local action_state = require "telescope.actions.state"
+local pickers = require 'telescope.pickers'
+local finders = require 'telescope.finders'
+local conf = require('telescope.config').values
+local actions = require 'telescope.actions'
+local action_state = require 'telescope.actions.state'
 
-local colors = function(opts)
-  opts = opts or {}
+function _G.telescope_find_dir(opts)
   pickers.new(opts, {
-    prompt_title = "Name",
-    finder = finders.new_table {
-      fn_command = function()
-        return
-        "find -L ~ ~/server/apps ~/code -path ~/.Trash -prune -o -type d -maxdepth 2 -print | fzf"
-      end,
-      entry_maker = function(entry)
-        return { value = entry, display = entry[1], ordinal = entry[1] }
-      end
-    },
+    prompt_title = 'Find Directory',
+    finder = finders.new_oneshot_job({
+      'find',
+      '-L',
+      vim.fn.expand('$HOME'),
+      vim.fn.expand('$HOME/code'),
+      vim.fn.expand('$HOME/server/apps'),
+      '-type', 'd',
+      '-maxdepth', '1'
+    }, opts),
     sorter = conf.file_sorter(opts),
-    attach_mappings = function(prompt_bufnr, map)
+    attach_mappings = function(prompt_bufnr)
       actions.select_default:replace(function()
-        actions.close(prompt_bufnr)
         local selection = action_state.get_selected_entry()
-        print(vim.inspect(selection))
-        vim.api.nvim_put({ selection[1] }, "", false, true)
+        if selection ~= nil then
+          actions.close(prompt_bufnr)
+          os.execute(vim.fn.expand('$HOME/.local/bin/tmux-sessioniser' .. ' ' .. selection[1]))
+        end
       end)
       return true
-    end
+    end,
   }):find()
 end
-
-colors(require("telescope.themes").get_dropdown {})
