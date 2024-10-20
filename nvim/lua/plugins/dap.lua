b89064a59fd830f6ede8494188aa9d3cd7c94cfe
Matt Thorning <matthewthorning@gmail.com>
2024-10-20 11:27:17 +0100

    dap improvements

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,6 +1,7 @@
 -- vim:foldmethod=marker
 --
 return {
+  {
    'mfussenegger/nvim-dap',
   config = function()
 
@@ -43,4 +44,29 @@ return {
     -- }}}
 
   end
+},
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+    config = function()
+      require'dapui'.setup()
+      local dap, dapui = require("dap"), require("dapui")
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+  end
+  }
 }
