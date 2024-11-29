bbe567d5923cd42652088cda61960e1f8d624d77
Matt Thorning <matt.thorning@grafana.com>
2024-11-29 16:49:30 +0000

    updates

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -42,7 +42,26 @@ return {
         args = {},
       },
     }
-    -- }}}
+  -- }}}
+
+  -- go {{{
+      -- see https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#go
+      dap.adapters.go = {
+        type = 'executable';
+        command = 'node';
+        args = {os.getenv('HOME') .. '/vscode-go/extension/dist/debugAdapter.js'};
+      }
+      dap.configurations.go = {
+        {
+          type = 'go';
+          name = 'Debug';
+          request = 'launch';
+          showLog = false;
+          program = "${file}";
+          dlvToolPath = vim.fn.exepath('dlv')  -- Adjust to where delve is installed
+        },
+     }
+  -- }}}
 
   end
 },
--- stowdirs/home/work/.zshrc
+++ stowdirs/home/work/.zshrc
@@ -4,6 +4,8 @@ if [ -f '/Users/mthorning/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/mthor
 # The next line enables shell command completion for gcloud.
 if [ -f '/Users/mthorning/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/mthorning/google-cloud-sdk/completion.zsh.inc'; fi
 
+eval "$(direnv hook zsh)"
+
 alias irmdb="kubectl exec -it mysql-0 -n devenv -- mysql --user=user --password=pass --database=grafana_incident"
 alias irmreset="make down && make cluster/down && make clean-dist && make cluster/up && make irm-local/up"
 
