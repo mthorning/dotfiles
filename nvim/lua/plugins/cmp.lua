a8da2acd3696ea95ea2ce2afd38ed88148813307
Matt Thorning <matthewthorning@gmail.com>
2026-01-21 21:24:37 +0000

    re-enable autocomplete

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -2,31 +2,11 @@ return {
   {
     'hrsh7th/nvim-cmp',
     event = 'VeryLazy',
-    dependencies = {
-      'dcampos/cmp-snippy',
-      {
-        'dcampos/nvim-snippy',
-        config = function()
-          require 'snippy'.setup {
-            mappings = {
-              is = {
-                ['<Tab>'] = 'expand_or_advance',
-                ['<S-Tab>'] = 'previous',
-              },
-            }
-          }
-        end
-      },
-    },
+    dependencies = {},
     config = function()
       local cmp = require 'cmp'
 
       cmp.setup {
-        snippet = {
-          expand = function(args)
-            require('snippy').expand_snippet(args.body)
-          end,
-        },
         mapping = {
           ['<Up>'] = cmp.mapping.scroll_docs(-4),
           ['<Down>'] = cmp.mapping.scroll_docs(4),
@@ -40,7 +20,13 @@ return {
           })
         },
         sources = {
-          { name = 'nvim_lsp' }, { name = 'buffer' }, { name = 'path' }, { name = 'snippy' }, {
+          {
+            name = 'nvim_lsp',
+            entry_filter = function(entry, ctx)
+              local kind = require('cmp.types.lsp').CompletionItemKind[entry:get_kind()]
+              return vim.tbl_contains({ 'Method', 'Function', 'Field', 'Property', 'Variable' }, kind)
+            end
+          }, { name = 'path' }, {
           name = 'buffer',
           options = {
             get_bufnrs = function()
@@ -58,7 +44,6 @@ return {
               path = 'ﱮ',
               buffer = '﬘',
               zsh = '',
-              snippy = '',
               spell = '暈'
             })[entry.source.name]
 
@@ -70,13 +55,11 @@ return {
   },
   {
     'hrsh7th/cmp-nvim-lsp',
-    enabled = false,
     event = 'BufReadPre',
     dependencies = { 'hrsh7th/nvim-cmp' }
   },
   {
     'hrsh7th/cmp-buffer',
-    enabled = false,
     event = 'BufReadPre',
     dependencies = { 'hrsh7th/nvim-cmp' }
   },
